6c1336b92746c76f14b2d8646255d24d56f5381c
iabdalkader <i.abdalkader@gmail.com>
2022-01-02 15:07:33 +0200

    rp2: Add support for DHT11 and DHT22 sensors.

--- ports/rp2/boards/manifest.py
+++ ports/rp2/boards/manifest.py
@@ -1,5 +1,6 @@
 freeze("$(BOARD_DIR)/modules")
 freeze("$(PORT_DIR)/modules")
 freeze("$(MPY_DIR)/drivers/onewire")
+freeze("$(MPY_DIR)/drivers/dht", "dht.py")
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
 include("$(MPY_DIR)/drivers/neopixel/manifest.py")
